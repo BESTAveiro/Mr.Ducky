df4f8f819101113460756784eaee5d87f5594186
Miguel Oliveira <migueloliveira16@ua.pt>
2017-01-12 21:52:15 +0000

    Spreadsheet parser testing with actual file.

--- MrDucky/src/main/java/pt/bestaveiro/mrducky/parsers/SpreadsheetParser.java
+++ MrDucky/src/main/java/pt/bestaveiro/mrducky/parsers/SpreadsheetParser.java
@@ -5,10 +5,41 @@
  */
 package pt.bestaveiro.mrducky.parsers;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  *
  * @author jeronimo
  */
 public class SpreadsheetParser {
     
+    public static void main(String[] args){        
+        
+        List<String> cols = new ArrayList<>();
+        
+        try {
+            JSONObject json = new JSONObject(IOUtils.toString(new URL("http://gsx2json.com/api?id=1SmD6cL4sdGhKSMBeFhn-VolvptKVy96kVRtwMun4uss"), Charset.forName("UTF-8")));
+            
+            JSONArray jsonarr = json.getJSONArray("rows");
+            
+            for(int i=0; i<jsonarr.length(); i++){
+                JSONObject obj = jsonarr.getJSONObject(i);
+                for(String param : obj.keySet()){
+                    System.out.println(param + obj.getString(param));
+                }
+            }
+            //System.out.println(json.toString());
+        } catch (IOException ex) {
+            Logger.getLogger(SpreadsheetParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
