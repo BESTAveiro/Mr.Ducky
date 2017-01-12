0444f889d67ffd302efcb684c7bd66eec6a24b65
andre.jeronimo <andre.jeronimo@ua.pt>
2017-01-12 19:51:04 +0000

    Added Anniversary class

--- MrDucky/src/main/java/pt/bestaveiro/mrducky/core/Anniversary.java
+++ MrDucky/src/main/java/pt/bestaveiro/mrducky/core/Anniversary.java
@@ -5,10 +5,47 @@
  */
 package pt.bestaveiro.mrducky.core;
 
+import java.util.Date;
+import java.util.Map;
+
 /**
  *
  * @author jeronimo
  */
 public class Anniversary {
+    
+    private String name;
+    private Date date;
+    private Map<String, String> tags;
+
+    public Anniversary(String name, Date date, Map<String, String> tags) {
+        this.name = name;
+        this.date = date;
+        this.tags = tags;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Map<String, String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Map<String, String> tags) {
+        this.tags = tags;
+    }
     
 }
