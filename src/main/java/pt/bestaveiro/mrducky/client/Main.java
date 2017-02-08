f9840292c50d1613a369fc3af2dbc96e9f9ce624
André Santos <andre.jeronimo@ua.pt>
2017-02-08 16:50:39 +0000

    Udated

--- src/main/java/pt/bestaveiro/mrducky/client/Main.java
+++ src/main/java/pt/bestaveiro/mrducky/client/Main.java
@@ -5,6 +5,9 @@
  */
 package pt.bestaveiro.mrducky.client;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  *
  * @author jeronimo
@@ -12,6 +15,10 @@ package pt.bestaveiro.mrducky.client;
 public class Main {
     
     public static void main(String[] args) {               
+        
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.println( sdf.format(cal.getTime()) );
         
         // Run anniversary process
         Tasks.sendAnniversaries();
